2aed6108d13805d306c2d36114d43035cf7ba6f2
Mohammed Ibrahim Farooqui <fmohammedibr@vmware.com>
2023-10-24 20:13:49 +0530

    Initial for for h2 support

--- src/main/java/org/reboot/server/secure/SockMain.java
+++ src/main/java/org/reboot/server/secure/SockMain.java
@@ -1,16 +1,10 @@
 package org.reboot.server.secure;
 
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.reboot.server.secure.core.IProxyRequestProcessor;
 import org.reboot.server.secure.core.IDestinationServerSocketProvider;
-import org.reboot.server.secure.model.InboundSocket;
-import org.reboot.server.secure.model.ManagedSocket;
-import org.reboot.server.secure.model.RequestContext;
-import org.reboot.server.secure.model.SessionHandle;
-import org.reboot.server.secure.model.TraceContext;
 import org.reboot.server.secure.server.IServerSocketProvider;
 import org.reboot.server.secure.util.IServerConfiguration;
+import org.reboot.server.secure.util.RequestHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +12,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
 
-import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLServerSocket;
-import javax.net.ssl.SSLSocket;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -35,25 +22,22 @@ public class SockMain {
 
   private static Logger log = LoggerFactory.getLogger(SockMain.class);
 
-  public static final String DEST_SERVER_HOST = "dest.server.host";
-  public static final String UPDATE_SERVER_HOST = "update.server.host.header";
-
-  private IServerConfiguration serverConfiguration;
-
-  private IDestinationServerSocketProvider destinationServerSocketProvider;
-
-  private IProxyRequestProcessor proxyRequestProcessor;
+  private RequestHelper requestHelper;
 
   private IServerSocketProvider serverSocketProvider;
   @Autowired
-  public SockMain(IServerConfiguration serverConfiguration,
-                  @Qualifier("plainDestinationServerSocketProvider") IDestinationServerSocketProvider destinationServerSocketProvider,
-                  IProxyRequestProcessor proxyRequestProcessor,
+  public SockMain(RequestHelper requestHelper,
                   IServerSocketProvider serverSocketProvider) {
-    this.serverConfiguration = serverConfiguration;
-    this.destinationServerSocketProvider = destinationServerSocketProvider;
-    this.proxyRequestProcessor = proxyRequestProcessor;
+    this.requestHelper = requestHelper;
     this.serverSocketProvider = serverSocketProvider;
+  }
+
+  public static void main(String[] args) throws Exception {
+    AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+    appContext.scan("org.reboot.server.secure");
+    appContext.refresh();
+    SockMain client = (SockMain) appContext.getBean("appEntry");
+    client.run();
   }
 
   public void run() throws Exception {
@@ -63,68 +47,17 @@ public class SockMain {
       Socket socket = sslServerSocket.accept();
 
       executorService.submit(() -> {
-        safeProcessRequestAndSendResponse(socket);
+        safeProcessNewRequest(socket);
       });
     }
   }
 
-  public static void main(String[] args) throws Exception {
-
-    AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
-    appContext.scan("org.reboot.server.secure");
-    appContext.refresh();
-    SockMain client = (SockMain) appContext.getBean("appEntry");
-    client.run();
-  }
-
-  private void safeProcessRequestAndSendResponse(Socket socket) {
+  private void safeProcessNewRequest(Socket socket) {
     try {
-      processRequestAndSendResponse(socket);
+      requestHelper.processNewRequest(socket);
     } catch (Exception e) {
       e.printStackTrace();
       log.error("There was a problem: ", e);
     }
-  }
-
-  private void processRequestAndSendResponse(Socket socket) throws Exception {
-    performHandShake((SSLSocket) socket);
-    ManagedSocket managedSocket = destinationServerSocketProvider.getDestinationSocket();
-    RequestContext requestContext = new RequestContext();
-    requestContext.setDestinationHostName(managedSocket.getHost());
-    requestContext.setUpdateHostHeader(serverConfiguration.getBooleanProperty(UPDATE_SERVER_HOST));
-
-    Pair<TraceContext, TraceContext> traceContextPair = getTraceContext(managedSocket);
-    SessionHandle sessionHandle = new SessionHandle(new InboundSocket(socket), managedSocket, traceContextPair.getLeft(), traceContextPair.getRight());
-    proxyRequestProcessor.start(requestContext, sessionHandle);
-  }
-
-  private static void performHandShake(SSLSocket socket) throws IOException {
-    SSLSocket sslSocket = socket;
-    SSLParameters sslParameters = sslSocket.getSSLParameters();
-    String[] serverAPs ={ "http/1.1", "two", "three" };
-    sslParameters.setApplicationProtocols(serverAPs);
-    sslSocket.setSSLParameters(sslParameters);
-    sslSocket.startHandshake();
-    String ap = sslSocket.getApplicationProtocol();
-    log.debug("Handshake successfull, with protocol agreed upon: {}", ap);
-  }
-
-  private Pair<TraceContext, TraceContext> getTraceContext(ManagedSocket managedSocket) throws Exception {
-    boolean traceEnabled = serverConfiguration.getBooleanProperty("http.tracing.enabled");
-    OutputStream requestStream = null;
-    OutputStream responseStream = null;
-
-    if (traceEnabled) {
-      String id = managedSocket.getConnectionId();
-      String requestId = id + "-REQ.log";
-      String responseId = id + "-RES.log";
-      String dataDumpDirectory = serverConfiguration.getRequiredProperty("data.dump.dir");
-      File outputFile = Paths.get(dataDumpDirectory, requestId).toFile();
-      requestStream = new FileOutputStream(outputFile);
-      File responseFile = Paths.get(dataDumpDirectory, responseId).toFile();
-      responseStream = new FileOutputStream(responseFile);
-    }
-
-    return Pair.of(new TraceContext(requestStream), new TraceContext(responseStream));
   }
 }
---
+++ src/main/java/org/reboot/server/secure/util/RequestHelper.java
@@ -0,0 +1,158 @@
+package org.reboot.server.secure.util;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.reboot.server.secure.core.IDestinationServerSocketProvider;
+import org.reboot.server.secure.core.IProxyRequestProcessor;
+import org.reboot.server.secure.model.HttpVersion;
+import org.reboot.server.secure.model.InboundSocket;
+import org.reboot.server.secure.model.ManagedSocket;
+import org.reboot.server.secure.model.RequestContext;
+import org.reboot.server.secure.model.SessionHandle;
+import org.reboot.server.secure.model.TraceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.SSLSocket;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RequestHelper {
+
+  public static final String HTTP_1_1_ENABLE = "http1.1.enable";
+  public static final String HTTP_2_ENABLE = "http2.enable";
+  private static Logger log = LoggerFactory.getLogger(RequestHelper.class);
+
+  public static final String UPDATE_SERVER_HOST = "update.server.host.header";
+
+  public static final String HTTP_PROTOCOL_V_1_1 = "http/1.1";
+  public static final String HTTP_PROTOCOL_V_2 = "h2";
+
+
+
+  private IServerConfiguration serverConfiguration;
+
+  private IDestinationServerSocketProvider destinationServerSocketProvider;
+
+  private IProxyRequestProcessor proxyRequestProcessor;
+
+  public RequestHelper(IServerConfiguration serverConfiguration,
+                       @Qualifier("plainDestinationServerSocketProvider") IDestinationServerSocketProvider destinationServerSocketProvider,
+                       IProxyRequestProcessor proxyRequestProcessor) {
+
+    this.serverConfiguration = serverConfiguration;
+    this.destinationServerSocketProvider = destinationServerSocketProvider;
+    this.proxyRequestProcessor = proxyRequestProcessor;
+  }
+
+  public void processNewRequest(Socket socket) throws Exception {
+    Optional<HttpVersion> httpVersionOptional = performHandShake((SSLSocket) socket);
+
+    if (httpVersionOptional.isPresent()) {
+      handleHttpVersionedRequest(httpVersionOptional.get(), socket);
+    }
+  }
+
+  private void handleHttpVersionedRequest(HttpVersion httpVersion, Socket socket) throws Exception {
+
+    switch (httpVersion) {
+      case HTTP_1_1:
+        initiateHttp11ProxyRequest(socket);
+        return;
+
+      case HTTP_2:
+        initiateHttp2ProxyRequest(socket);
+    }
+  }
+
+  private void initiateHttp11ProxyRequest(Socket socket) throws Exception {
+    ManagedSocket managedSocket = destinationServerSocketProvider.getDestinationSocket();
+    RequestContext requestContext = new RequestContext();
+    requestContext.setDestinationHostName(managedSocket.getHost());
+    requestContext.setUpdateHostHeader(serverConfiguration.getBooleanProperty(UPDATE_SERVER_HOST));
+
+    Pair<TraceContext, TraceContext> traceContextPair = getTraceContext(managedSocket);
+    SessionHandle sessionHandle = new SessionHandle(new InboundSocket(socket), managedSocket, traceContextPair.getLeft(), traceContextPair.getRight());
+    proxyRequestProcessor.start(requestContext, sessionHandle);
+  }
+
+  private Optional<HttpVersion> performHandShake(SSLSocket socket) throws IOException {
+    try {
+      SSLSocket sslSocket = socket;
+      SSLParameters sslParameters = sslSocket.getSSLParameters();
+
+      List<String> serverSupportedALPN = new ArrayList<>();
+
+      if (serverConfiguration.getBooleanProperty(HTTP_1_1_ENABLE)) {
+        serverSupportedALPN.add(HTTP_PROTOCOL_V_1_1);
+      }
+
+      if (serverConfiguration.getBooleanProperty(HTTP_2_ENABLE)) {
+        serverSupportedALPN.add(HTTP_PROTOCOL_V_2);
+      }
+
+      if (CollectionUtils.isEmpty(serverSupportedALPN)) {
+        throw new RuntimeException("Protocols supported set is empty");
+      }
+
+      sslParameters.setApplicationProtocols(serverSupportedALPN.toArray(new String[0]));
+      sslSocket.setSSLParameters(sslParameters);
+      sslSocket.startHandshake();
+      String agreedOnProtocol = sslSocket.getApplicationProtocol();
+      log.debug("Handshake successful, with protocol agreed upon: {}", agreedOnProtocol);
+
+      if (HTTP_PROTOCOL_V_1_1.equalsIgnoreCase(agreedOnProtocol)) {
+        return Optional.of(HttpVersion.HTTP_1_1);
+      } else if (HTTP_PROTOCOL_V_2.equalsIgnoreCase(agreedOnProtocol)) {
+        return Optional.of(HttpVersion.HTTP_2);
+      }
+
+      log.error("Unsupported protocol: {}", agreedOnProtocol);
+      return Optional.empty();
+    } catch (Exception e) {
+      log.error("There was a problem while handshake.", e);
+      IOUtils.closeQuietly(socket);
+      return Optional.empty();
+    }
+  }
+
+  private Pair<TraceContext, TraceContext> getTraceContext(ManagedSocket managedSocket) throws Exception {
+    boolean traceEnabled = serverConfiguration.getBooleanProperty("http.tracing.enabled");
+    OutputStream requestStream = null;
+    OutputStream responseStream = null;
+
+    if (traceEnabled) {
+      String id = managedSocket.getConnectionId();
+      String requestId = id + "-REQ.log";
+      String responseId = id + "-RES.log";
+      String dataDumpDirectory = serverConfiguration.getRequiredProperty("data.dump.dir");
+      File outputFile = Paths.get(dataDumpDirectory, requestId).toFile();
+      requestStream = new FileOutputStream(outputFile);
+      File responseFile = Paths.get(dataDumpDirectory, responseId).toFile();
+      responseStream = new FileOutputStream(responseFile);
+    }
+
+    return Pair.of(new TraceContext(requestStream), new TraceContext(responseStream));
+  }
+
+  private void initiateHttp2ProxyRequest(Socket socket) throws Exception {
+    byte[] buffer = new byte[1024*8];
+    int read = socket.getInputStream().read(buffer, 0, buffer.length);
+    log.info("Obtained packet: {}, number of bytes read: {}", new String(buffer, 0, read), read);
+    char[] hexChar = Hex.encodeHex(buffer, 0, read, true);
+    log.info("Hex char: [{}]", hexChar);
+  }
+}
